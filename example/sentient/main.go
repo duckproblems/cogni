8cd55e9548101627dd9b4f1a4e330c1c83fef266
André devkcud Albanese Junior <patommmmm@proton.me>
2025-04-02 19:33:05 -0300

    chore(examples): Update example to use new animation framework

--- example/sentient/main.go
+++ example/sentient/main.go
@@ -1,106 +1,48 @@
 package main
 
 import (
-	"math"
-	"slices"
-
 	"github.com/duckproblems/cogni/core"
 	"github.com/duckproblems/cogni/core/ecs"
 	"github.com/duckproblems/cogni/core/ecs/components"
 	"github.com/duckproblems/cogni/utils"
-	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/duckproblems/cogni/utils/graphics"
 )
 
 func main() {
 	game := core.NewGame(*core.NewWindow(core.Window{Title: "Sentient", Geometry: utils.Pair[int]{Left: 800, Right: 600}}))
 
-	playerImage, _ := utils.LoadFromPath("./assets/sprites/player.png", 16, 16)
-
-	runningDown := []*ebiten.Image{
-		playerImage.SelectSubImage(1, 1),
-		playerImage.SelectSubImage(1, 2),
-		playerImage.SelectSubImage(1, 3),
-		playerImage.SelectSubImage(1, 4),
-	}
-	runningUp := []*ebiten.Image{
-		playerImage.SelectSubImage(2, 1),
-		playerImage.SelectSubImage(2, 2),
-		playerImage.SelectSubImage(2, 3),
-		playerImage.SelectSubImage(2, 4),
-	}
-	runningLeft := []*ebiten.Image{
-		playerImage.SelectSubImage(3, 1),
-		playerImage.SelectSubImage(3, 2),
-		playerImage.SelectSubImage(3, 3),
-		playerImage.SelectSubImage(3, 4),
-	}
-	runningRight := []*ebiten.Image{
-		playerImage.SelectSubImage(4, 1),
-		playerImage.SelectSubImage(4, 2),
-		playerImage.SelectSubImage(4, 3),
-		playerImage.SelectSubImage(4, 4),
-	}
+	playerSpriteSheet, _ := graphics.LoadSpriteSheet("./assets/sprites/player.png", 16, 16)
+	playerAnimSet := graphics.NewAnimationSet()
+	playerAnimSet.AddAnimation(graphics.AnimStateWalkUp, playerSpriteSheet.GetFrames(2, 1, 2, 4))
+	playerAnimSet.AddAnimation(graphics.AnimStateWalkDown, playerSpriteSheet.GetFrames(1, 1, 1, 4))
+	playerAnimSet.AddAnimation(graphics.AnimStateWalkLeft, playerSpriteSheet.GetFrames(3, 1, 3, 4))
+	playerAnimSet.AddAnimation(graphics.AnimStateWalkRight, playerSpriteSheet.GetFrames(4, 1, 4, 4))
 
 	player := ecs.NewEntity("Player")
 	player.AddComponent(&components.Sprite{
-		Frames:     runningDown,
+		Frames:     playerSpriteSheet.GetFrames(1, 1, 1, 1),
 		FrameSpeed: 5.,
 		Loop:       true,
-		Playing:    false,
+		Playing:    true,
 	})
-	player.AddComponent(&components.Transform{X: 30, Y: 30, ScaleX: 1, ScaleY: 1, Rotation: 0})
+	player.AddComponent(&components.Transform{X: 30, Y: 30, ScaleX: 5, ScaleY: 5, Rotation: 0})
+	player.AddComponent(&components.Input{})
 
 	var sprite *components.Sprite
 	player.GetComponent(&sprite)
 
-	updateAnimationDirection := func(vX, vY float64) {
-		absX := math.Abs(vX)
-		absY := math.Abs(vY)
-
-		var targetFrames []*ebiten.Image
-
-		if absX >= absY {
-			if vX < 0 {
-				targetFrames = runningLeft
-			} else {
-				targetFrames = runningRight
-			}
-		} else {
-			if vY < 0 {
-				targetFrames = runningUp
-			} else {
-				targetFrames = runningDown
-			}
-		}
-
-		if !slices.Equal(sprite.Frames, targetFrames) && targetFrames != nil {
-			sprite.Frames = targetFrames
-			sprite.CurrentFrame = 0
-			sprite.Playing = true
-		}
-	}
-
-	player.AddComponent(&components.Input{})
 	player.AddComponent(&components.Movement{
 		MaxSpeed:     100.,
 		Acceleration: 1000.,
 		Friction:     0,
 		OnStartedMoving: func(vX, vY float64) {
-			sprite.Playing = true
-			updateAnimationDirection(vX, vY)
+			graphics.UpdateMovementAnimation(sprite, playerAnimSet, vX, vY)
 		},
 		OnStoppedMoving: func(vX, vY float64) {
-			sprite.Playing = false
-			sprite.CurrentFrame = 0
-		},
-		OnAccelerating: func(vX, vY float64) {
-			updateAnimationDirection(vX, vY)
-		},
-		OnDecelerating: func(vX, vY float64) {
-			updateAnimationDirection(vX, vY)
+			graphics.UpdateMovementAnimation(sprite, playerAnimSet, vX, vY)
 		},
 		OnCruising: func(vX, vY float64) {
-			updateAnimationDirection(vX, vY)
+			graphics.UpdateMovementAnimation(sprite, playerAnimSet, vX, vY)
 		},
 	})
 
